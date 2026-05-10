056d12c7514eab0669ad3ce5d58c525c3918d71d
Luca Alberto <francoluca730@gmail.com>
2026-05-09 21:37:39 -0300

    integro google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const siteTitle = "Los Hermanos | Agua desmineralizada";
 const siteDescription =
   "Producción y distribución de agua desmineralizada para talleres, estaciones de servicio y comercios.";
 const siteUrl = getBaseUrl();
+const googleTagManagerId = "GTM-PHPVLZ9B";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
@@ -41,7 +42,28 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="es">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${googleTagManagerId}');
+            `
+          }}
+        />
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <WhatsAppFloatingButton />
       </body>
